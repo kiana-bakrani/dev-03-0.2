3b713ad329540cc64e383fb46ccc207c32bfefc8
Kiana Bakrani Balani <kianabakrani@gmail.com>
2025-10-02 22:04:54 -0700

    Programming Language

--- src/main/java/cs151/application/ProgrammingLanguage.java
+++ src/main/java/cs151/application/ProgrammingLanguage.java
@@ -1,4 +1,16 @@
 package cs151.application;
 
 public class ProgrammingLanguage {
+    private String name;
+
+    public ProgrammingLanguage() {}
+
+    public ProgrammingLanguage(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    @Override public String toString() { return name; }
 }
